10fa840a9a5e1eecca65f7b217c1096e5531ffae
shimo-nu <shimo@ucl.nuee.nagoya-u.ac.jp>
2023-05-21 12:30:53 +0900

    initial commit

---
+++ cpluspod_joycontroller/joy2twist.py
@@ -0,0 +1,61 @@
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import Joy
+from sensor_msgs.msg import JointState
+from geometry_msgs.msg import Twist
+
+import rich
+from rich.console import Console
+
+console = Console()
+
+class joycon_controller(Node):
+    def __init__(self):
+        super().__init__('joycon_controller')
+        self.twist_pub = self.create_publisher(Twist, "/cmd_vel", 100)
+
+        self.joy_sub = self.create_subscription(Joy, "/joy", self.joy_callback, 10)
+        
+        self.isPublishTwist = False
+
+        self.declare_parameter('linear_x_offset', 1.0)
+        self.declare_parameter('angular_z_offset', 1.0)
+
+        self.linear_x_offset = self.get_parameter('linear_x_offset').get_parameter_value().double_value
+        self.angular_z_offset = self.get_parameter('angular_z_offset').get_parameter_value().double_value
+
+    def joy_callback(self, msgs):
+        twist = Twist()
+        twist.linear.x = msgs.axes[1] * self.linear_x_offset
+        twist.angular.z = msgs.axes[0] * self.angular_z_offset
+        
+        # stop to publish twist
+        # A : Publish
+        # B : Not Publish
+        if (msgs.buttons[0]):
+            self.isPublishTwist = True
+        elif (msgs.buttons[1]):
+            self.isPublishTwist = False
+
+        if (self.isPublishTwist):
+            self.twist_pub.publish(twist)
+
+        # X : Info 
+        if (msgs.button[3]):
+            console.log("linear offset : {}".format(self.linear_x_offset))
+            console.log("angular offset : {}".format(self.angular_z_offset))
+            console.log("Twist : {}".format('True' if self.isPublishTwist else 'False'))
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    joycon_controller_class = joycon_controller()
+    rclpy.spin(joycon_controller_class)
+    joycon_controller.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from glob import glob
+
+package_name = 'cpluspod_joycontroller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/', glob('launch/*.launch.xml')),
+    ],
+    py_modules=[
+        'cpluspod_joycontroller.joy2twist',
+        ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tmistudent',
+    maintainer_email='shimo@ucl.nuee.nagoya-u.ac.jp',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'cpluspod_joycontroller = cpluspod_joycontroller.joy2twist:main'
+        ],
+    },
+)
