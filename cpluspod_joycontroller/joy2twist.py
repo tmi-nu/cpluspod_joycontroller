2ff61ee8d354f9340efc2335d3c2a4daebcc4f83
hosogaya <hosogaya.hirotaka@gmail.com>
2023-05-21 14:15:09 +0900

    twist to ackermann

--- cpluspod_joycontroller/joy2twist.py
+++ cpluspod_joycontroller/joy2twist.py
@@ -4,6 +4,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import Joy
 from sensor_msgs.msg import JointState
 from geometry_msgs.msg import Twist
+from autoware_auto_control_msgs.msg import AckermannControlCommand
 
 import rich
 from rich.console import Console
@@ -13,7 +14,7 @@ console = Console()
 class joycon_controller(Node):
     def __init__(self):
         super().__init__('joycon_controller')
-        self.twist_pub = self.create_publisher(Twist, "/cmd_vel", 100)
+        self.twist_pub = self.create_publisher(AckermannControlCommand, "/control/command/control_cmd", 100)
 
         self.joy_sub = self.create_subscription(Joy, "/joy", self.joy_callback, 10)
         
@@ -26,9 +27,10 @@ class joycon_controller(Node):
         self.angular_z_offset = self.get_parameter('angular_z_offset').get_parameter_value().double_value
 
     def joy_callback(self, msgs):
-        twist = Twist()
-        twist.linear.x = msgs.axes[1] * self.linear_x_offset
-        twist.angular.z = msgs.axes[0] * self.angular_z_offset
+        # twist = Twist()
+        ackermann = AckermannControlCommand()
+        ackermann.longitudinal.speed =  msgs.axes[1] * self.linear_x_offset
+        ackermann.lateral.steering_tire_angle = msgs.axes[0] * self.angular_z_offset
         
         # stop to publish twist
         # A : Publish
@@ -39,10 +41,10 @@ class joycon_controller(Node):
             self.isPublishTwist = False
 
         if (self.isPublishTwist):
-            self.twist_pub.publish(twist)
+            self.twist_pub.publish(ackermann)
 
         # X : Info 
-        if (msgs.button[3]):
+        if (msgs.buttons[3]):
             console.log("linear offset : {}".format(self.linear_x_offset))
             console.log("angular offset : {}".format(self.angular_z_offset))
             console.log("Twist : {}".format('True' if self.isPublishTwist else 'False'))
